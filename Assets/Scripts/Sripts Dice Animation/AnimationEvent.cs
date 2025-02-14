efe4bd96fc50fbaa085affecbba10ba7367b6972
victorbr2 <47553336+victorbr2@users.noreply.github.com>
2025-02-14 11:13:46 +0100

    Cambios Combate

--- Assets/Scripts/Sripts Dice Animation/AnimationEvent.cs
+++ Assets/Scripts/Sripts Dice Animation/AnimationEvent.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
-
+using System.Collections;
 public class AnimationEvent : MonoBehaviour
 {
     [SerializeField] private DiceMeshController dice;
+    [SerializeField] private GameObject CombatScene;
+    [SerializeField] private GameObject RawCombatScene;
 
     public void OnAnimationEnd()
     {
         
         Debug.Log("Acaba animacion");
-
+        CombatScene.SetActive(false);
+        StartCoroutine(DelayActions());
     }
-    
+    private IEnumerator DelayActions()
+    {
+        // Esperamos 2 segundos
+        yield return new WaitForSeconds(2f);
+      
+        RawCombatScene.SetActive(false);
+    }
 }
